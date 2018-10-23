05877ad697ca6d7fcf351d42f4ea589b0f1b5290
Unknown <9uilhermealencar@gmail.com>
2018-10-22 21:50:33 -0300

    GUI-1 consulta form

--- familycare-front-end/src/app/principal/consulta/consulta.module.ts
+++ familycare-front-end/src/app/principal/consulta/consulta.module.ts
@@ -2,13 +2,15 @@ import { ConsultaService } from './consulta.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsultaComponent } from './consulta.component';
+import { ConsultaFormComponent } from './consulta-form/consulta-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    ConsultaComponent
+    ConsultaComponent,
+    ConsultaFormComponent
   ],
   providers: [
     ConsultaService
