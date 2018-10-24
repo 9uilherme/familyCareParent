ded4613735943a257d62b2b453dbfea574549bf0
wendelguedes <wendelguedes@gmail.com>
2018-10-24 20:27:39 -0300

    Adicionado pattern para os campos do tipo date e time

--- familycare-back-end/src/main/java/br/ufg/familycare/model/Medicamento.java
+++ familycare-back-end/src/main/java/br/ufg/familycare/model/Medicamento.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.ufg.familycare.enums.EnumIntervalo;
 import br.ufg.familycare.enums.EnumUnidade;
 import lombok.Data;
@@ -26,21 +28,23 @@ public class Medicamento {
 	private String nome;
 
 	@Temporal(TemporalType.DATE)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
 	private Date data;
-	
+
 	@Temporal(TemporalType.TIME)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
 	private Date hora;
-	
+
 	private Boolean lembrete;
-	
+
 	private BigDecimal dosagem;
-	
+
 	@Enumerated(EnumType.STRING)
 	private EnumUnidade unidade;
-	
+
 	@Enumerated(EnumType.STRING)
 	private EnumIntervalo intervalo;
-	
+
 	private Integer quantidadeDias;
 
 }
