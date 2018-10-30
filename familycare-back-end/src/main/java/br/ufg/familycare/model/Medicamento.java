fc2f3e444be568eb2c85bb7bcee2626196561f10
wendelguedes <wendelguedes@gmail.com>
2018-10-29 21:30:16 -0300

    Criação dos controllers para listagem dos enuns: sexo, tipoSanguineo e fatorRh

--- familycare-back-end/src/main/java/br/ufg/familycare/model/Medicamento.java
+++ familycare-back-end/src/main/java/br/ufg/familycare/model/Medicamento.java
@@ -1,13 +1,16 @@
 package br.ufg.familycare.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -16,23 +19,29 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import br.ufg.familycare.enums.EnumIntervalo;
 import br.ufg.familycare.enums.EnumUnidade;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-public class Medicamento {
+@NoArgsConstructor
+public class Medicamento implements Serializable {
 
-	@Id 
+	private static final long serialVersionUID = 1L;
+
+	@Id
 	@GeneratedValue
 	private Long id;
 
 	private String nome;
 
+	@ManyToOne
+	private Membro membro;
+
 	@Temporal(TemporalType.DATE)
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private Date data;
 
 	@Temporal(TemporalType.TIME)
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
 	private Date hora;
 
 	private Boolean lembrete;
--- familycare-back-end/src/main/java/br/ufg/familycare/model/Membro.java
+++ familycare-back-end/src/main/java/br/ufg/familycare/model/Membro.java
@@ -1,5 +1,6 @@
 package br.ufg.familycare.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -8,30 +9,38 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Type;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.ufg.familycare.enums.EnumFatorRH;
 import br.ufg.familycare.enums.EnumSexo;
 import br.ufg.familycare.enums.EnumTipoSanguineo;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-public class Membro {
+@NoArgsConstructor
+public class Membro implements Serializable {
 
-	@Id 
+	private static final long serialVersionUID = 1L;
+
+	@Id
 	@GeneratedValue
 	private Long id;
 
-	@NotNull(message="{data-nascimento.notnull}")
+	@Temporal(TemporalType.DATE)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private Date dataNascimento;
 
-	@NotBlank(message="{nome.notblank}")
-	@Size(min=1,max=255, message="{nome.size}")
+	@NotBlank(message = "{nome.notblank}")
+	@Size(min = 1, max = 255, message = "{nome.size}")
 	private String nome;
 
 	private BigDecimal peso;
