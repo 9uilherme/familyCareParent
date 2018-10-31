2b2bea04a8a306c77cba478956b7eaa37bc0360b
wendelguedes <wendelguedes@gmail.com>
2018-10-31 18:31:52 -0300

    Criacao do dto UsuarioDto e os metodos para consulta e update de usuario

--- familycare-back-end/src/main/java/br/ufg/familycare/controller/UsuarioController.java
+++ familycare-back-end/src/main/java/br/ufg/familycare/controller/UsuarioController.java
@@ -1,24 +1,29 @@
 package br.ufg.familycare.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ufg.familycare.controller.dto.UsuarioDto;
 import br.ufg.familycare.model.Usuario;
 import br.ufg.familycare.service.UsuarioService;
 import io.swagger.annotations.Api;
 
 @RestController
+@RequestMapping("/usuarios")
 @Api(value = "Guidelines", description = "Cadastro de Usuários")
 public class UsuarioController {
 
@@ -28,35 +33,58 @@ public class UsuarioController {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
-	@PostMapping("/usuarios")
+	@PostMapping()
 	Usuario cadastrar(@Valid @RequestBody Usuario usuario) {
 		usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
 		return usuarioService.salvar(usuario);
 	}
 
-	@GetMapping("/usuarios/{id}")
-	Usuario consultarPorId(@PathVariable Long id) {
-		return usuarioService.consultarPorId(id).get();
-	}
-
-	@GetMapping("/usuarios/existe/{email}")
+	@GetMapping("/existe/{email}")
 	Boolean usuarioExiste(@PathVariable String email) {
-		Usuario usuario =  usuarioService.consultarPorEmail(email);
+		Usuario usuario = usuarioService.consultarPorEmail(email);
 		return usuario != null;
 	}
 
-	@PutMapping("/usuarios/{id}")
-	Usuario alterarUsuario(@Valid Usuario usuario, @PathVariable Long id) {
-		usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
-		return usuarioService.salvar(usuario);
+	@GetMapping("/{email}")
+	UsuarioDto consultarPorEmail(@PathVariable String email) {
+		Usuario usuario = usuarioService.consultarPorEmail(email);
+		return preencherUsuarioDto(usuario);
 	}
 
-	@GetMapping("/usuarios")
+	@PutMapping()
+	UsuarioDto alterarUsuario(@Valid @RequestBody UsuarioDto usuarioDto) {
+		Optional<Usuario> usuarioOpt = usuarioService.consultarPorId(usuarioDto.getId());
+		Usuario usuario = null;
+
+		if (usuarioOpt.isPresent()) {
+			usuario = usuarioOpt.get();
+
+			if (!StringUtils.isEmpty(usuarioDto.getNome())) {
+				usuario.setNome(usuarioDto.getNome());
+			}
+			if (!StringUtils.isEmpty(usuarioDto.getImagemPerfil())) {
+				usuario.setImagemPerfil(usuarioDto.getImagemPerfil());
+			}
+			if (!StringUtils.isEmpty(usuarioDto.getPassword())) {
+				usuario.setPassword(passwordEncoder.encode(usuarioDto.getPassword()));
+			}
+			usuario = usuarioService.salvar(usuario);
+			return preencherUsuarioDto(usuario);
+		} else {
+			return usuarioDto;
+		}
+	}
+
+	private UsuarioDto preencherUsuarioDto(Usuario usuario) {
+		return new UsuarioDto(usuario.getId(), usuario.getNome(), usuario.getPassword(), usuario.getImagemPerfil(),usuario.getPerfil(), usuario.getEmail());
+	}
+
+	@GetMapping()
 	List<Usuario> listarTodos() {
 		return usuarioService.listarTodos();
 	}
 
-	@DeleteMapping("/usuarios/{id}")
+	@DeleteMapping("/{id}")
 	void deletarPorId(@PathVariable Long id) {
 		usuarioService.deletarPorId(id);
 	}
---
+++ familycare-back-end/src/main/java/br/ufg/familycare/controller/dto/UsuarioDto.java
@@ -0,0 +1,28 @@
+package br.ufg.familycare.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import br.ufg.familycare.enums.EnumPerfil;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UsuarioDto {
+
+	private Long id;
+
+	private String nome;
+
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private String password;
+
+	private String imagemPerfil;
+	
+	private EnumPerfil perfil;
+	
+	private String email;
+}
