e12373328484895c7d5664a7df50573423c9d6be
wendelguedes <wendelguedes@gmail.com>
2018-11-03 00:09:03 -0300

    Finalização do componente nova-consulta

--- familycare-front-end/src/app/principal/consulta/nova-consulta/nova-consulta.component.ts
+++ familycare-front-end/src/app/principal/consulta/nova-consulta/nova-consulta.component.ts
@@ -1,88 +1,179 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { Consulta, Membro, Profissional } from '../consulta';
-import { ConsultaService } from './../consulta.service';
+import { Profissional } from '../../profissional/profissional';
+import { Membro } from '../../membro/membro';
+import { ConsultaService } from '../consulta.service';
+import { MembroService } from '../../membro/membro.service';
+import { ProfissionalService } from '../../profissional/profissional.service';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
+import { Consulta } from '../consulta';
 
 @Component({
-  selector: 'app-consulta-form',
   templateUrl: './nova-consulta.component.html',
   styleUrls: ['./nova-consulta.component.css']
 })
 export class NovaConsultaComponent implements OnInit {
-
-  consultaForm: FormGroup;
-  message: any = {};
-  classCss: any = {};
-  submitted: boolean = false;
-  consulta: Consulta;
-
-  constructor(private route: ActivatedRoute,
-    private formBuilder: FormBuilder,
-    private consultaService: ConsultaService
-  ) {
-    this.consulta = new Consulta();
-    this.consulta.membro = new Membro();
-    this.consulta.profissional = new Profissional();
-  }
-
-  ngOnInit(): void {
-    let id: number = this.route.snapshot.params['id'];
-    if (id > 0) {
-      this.consultarPorId(id);
+  
+    consultaForm: FormGroup;
+    message:any = {};
+    classCss:any = {};
+    submited:boolean = false;
+    membros:Membro[] = [];
+    profissionais:Profissional[] = [];
+    @ViewChild('inputDescricao') inputDescricao: ElementRef<HTMLInputElement>;
+  
+    constructor(
+        private formBuilder: FormBuilder,
+        private consultaService: ConsultaService,
+        private membroService: MembroService,
+        private profissionalService: ProfissionalService,
+        private route: ActivatedRoute,
+        private platformDetectorService: PlatformDetectorService
+      ){}
+  
+      ngOnInit(): void {
+        this.inicializarFormProfissional();
+        this.listarMembros();
+        this.listarProfissionais();
+  
+        let id:number = this.route.snapshot.params['id'];
+  
+        if(id > 0){
+          this.consultarPorId(id);
+        }
+  
+        if(this.platformDetectorService.isPlatformBrowser){
+            this.inputDescricao.nativeElement.focus();
+        }
     }
-    this.consultaForm = this.formBuilder.group({
-      membro: ['',
-        [
-          Validators.required,
-          Validators.minLength(1),
-          Validators.maxLength(255)
-        ]
-      ],
-      profissional: ['',
-        [
-          Validators.required,
-          Validators.minLength(1),
-          Validators.maxLength(255)
-        ]
-      ]
+  
+    inicializarFormProfissional(){
+      this.consultaForm = this.formBuilder.group({
+        id:[],
+        descricao: ['', [ Validators.required, Validators.minLength(1),Validators.maxLength(255) ]],
+        membro: ['', [ Validators.required ]],
+        profissional: ['', [ Validators.required ]],
+        endereco: ['', [ Validators.maxLength(255) ]],
+        data: [new Date(), [ Validators.required ]],
+        hora: [new Date(), [ Validators.required]],
+        lembrete:[true, [ Validators.required ]]
+      });
+    }
+  
+    consultarPorId(id:number){
+      this.consultaService.consultarPorId(id).subscribe((consulta:Consulta) => {
+        this.consultaForm.setValue({
+          id: consulta.id,
+          descricao: consulta.descricao,
+          membro: consulta.membro,
+          profissional: consulta.profissional,
+          endereco: consulta.endereco,
+          data: new Date(consulta.data),
+          hora: this.montarData(consulta.hora),
+          lembrete: consulta.lembrete
+        });
+    } , err => {
+      this.showMessage({
+        type: 'error',
+        text: err['error']['errors'][0]
+      });
     });
+    }
+  
+    montarData(hora:any):Date {
+      let horas = hora.split(":");
+      let data:Date = new Date();
+      data.setHours(horas[0]);
+      data.setMinutes(horas[1]);
+      data.setSeconds(horas[2]);
+      return data;
+    }
+  
+    compareMembro(m1:Membro, m2:Membro) {
+     if(m1 != null && m2 != null){
+       return m1.id === m2.id;
+     }
+     return false;
+    }
 
-  }
-  consultarPorId(id: number) {
-    console.log(id);
-  }
-  salvar() {
-    this.submitted = true;
-    this.consultaService.salvar(this.consulta).
-      subscribe(
-        res => console.log(res),
-        exception => console.log(exception))
-  }
+    compareProfissional(p1:Profissional, p2:Profissional) {
+      if(p1 != null && p2 != null){
+        return p1.id === p2.id;
+      }
+      return false;
+     }
+  
+    listarMembros(){
+      this.membroService.listarTodos().subscribe((membros:Membro[]) => {
+        this.membros = membros;
+      } , err => {
+        this.showMessage({
+          type: 'error',
+          text: err['error']['errors'][0]
+        });
+      });
+    }
 
-  get f() {
-    return this.consultaForm.controls;
+    listarProfissionais(){
+      this.profissionalService.listarTodos().subscribe((profissionais:Profissional[]) => {
+        this.profissionais = profissionais;
+      } , err => {
+        this.showMessage({
+          type: 'error',
+          text: err['error']['errors'][0]
+        });
+      });
+    }
+  
+    get f() { 
+        return this.consultaForm.controls; 
+    }
+  
+    salvar(){
+        this.submited = true;
+        let consulta = this.consultaForm.getRawValue() as Consulta;
+        this.consultaService.salvar(consulta)
+        .subscribe((consulta: Consulta) => {
+          this.consultaForm.reset({
+            data: new Date(),
+            hora: new Date(),
+          });
+          this.submited = false;
+          this.showMessage({
+            type: 'success',
+            text: `Registered ${consulta.descricao} successfully`
+          });
+      } , err => {
+        this.showMessage({
+          type: 'error',
+          text: err['error']['errors'] != undefined 
+          ? err['error']['errors'][0] 
+          : err['error'].message
+        });
+      });
+    }
+  
+    getFormGroupClass(isInvalid: boolean, isDirty:boolean): {} {
+      return {
+        'form-group': true,
+        'has-error' : isInvalid  && isDirty,
+        'has-success' : !isInvalid  && isDirty
+      };
+    }
+  
+    private showMessage(message: {type: string, text: string}): void {
+        this.message = message;
+        this.buildClasses(message.type);
+        setTimeout(() => {
+          this.message = undefined;
+        }, 3000);
+    }
+  
+    private buildClasses(type: string): void {
+       this.classCss = {'alert': true}
+       this.classCss['alert-'+type] = true;
+    }
+  
   }
-
-  getFormGroupClass(isInvalid: boolean, isDirty: boolean): {} {
-    return {
-      'form-group': true,
-      'has-error': isInvalid && isDirty,
-      'has-success': !isInvalid && isDirty
-    };
-  }
-
-  private showMessage(message: { type: string, text: string }): void {
-    this.message = message;
-    this.buildClasses(message.type);
-    setTimeout(() => {
-      this.message = undefined;
-    }, 3000);
-  }
-
-  private buildClasses(type: string): void {
-    this.classCss = { 'alert': true }
-    this.classCss['alert-' + type] = true;
-  }
-
-}
+  
