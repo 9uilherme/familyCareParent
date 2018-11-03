9e4f0946f5625ad1ff483383144e64bf5b293dce
wendelguedes <wendelguedes@gmail.com>
2018-11-03 00:10:30 -0300

    Remoção do componente consulta para utilização dos componentes nova-consulta e list-consulta

--- familycare-front-end/src/app/principal/consulta/consulta.service.ts
+++ familycare-front-end/src/app/principal/consulta/consulta.service.ts
@@ -7,34 +7,30 @@ import { Consulta } from './consulta';
 @Injectable()
 export class ConsultaService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient){}
 
-  listar(): Observable<any> {
-    return this.http.get(`${API_URL}consultas`);
-  }
-  
-  salvar(consulta: Consulta) {
-    return this.http.post(`${API_URL}consultas`, consulta);
-  }
+    salvar(consulta: Consulta){
+        return this.http.post(`${API_URL}consultas`, consulta);
+    }
 
-  listarTodos() {
-    return this.http.get(`${API_URL}consultas`);
-  }
+    listarTodos(){
+        return this.http.get(`${API_URL}consultas`);
+    }
 
-  listarComPaginacao(pagina: number, tamanho: number) {
-    return this.http.get(`${API_URL}consultas/${pagina}/${tamanho}`);
-  }
+    listarComPaginacao(pagina:number,tamanho:number){
+        return this.http.get(`${API_URL}consultas/${pagina}/${tamanho}`);
+    }
 
-  listarComFiltro(pagina: number, tamanho: number, nomeFilter: string) {
-    return this.http.get(`${API_URL}consultas/${pagina}/${tamanho}/${nomeFilter}`);
-  }
+    listarComFiltro(pagina:number,tamanho:number,descricaoFilter:string){
+        return this.http.get(`${API_URL}consultas/${pagina}/${tamanho}/${descricaoFilter}`);
+    }
 
-  delete(id: string) {
-    return this.http.delete(`${API_URL}consultas/${id}`);
-  }
+    delete(id:string){
+        return this.http.delete(`${API_URL}consultas/${id}`);
+    }
 
-  consultarPorId(id: number) {
-    return this.http.get(`${API_URL}consultas/${id}`);
-  }
+    consultarPorId(id:number){
+        return this.http.get(`${API_URL}consultas/${id}`);
+    }
 
 }
--- familycare-front-end/src/app/principal/consulta/consulta.ts
+++ familycare-front-end/src/app/principal/consulta/consulta.ts
@@ -1,20 +1,14 @@
-export class Membro {
-    nome: string;
-}
-export class Profissional {
-    nome: string;
-}
-export class Consulta {
+import { Membro } from '../membro/membro';
+import { Profissional } from '../profissional/profissional';
+
+export interface Consulta {
 
     id:number;
-    data: Date;
-    hora: Date;
+    data:Date,
+    hora:Date,
     descricao: string;
     endereco: string;
-    lembrar: boolean;
-    // Criar entidade de membro
+    lembrete: boolean;
     membro: Membro;
-    // Criat entidade de profissional
     profissional: Profissional;
-
 }
